5215b7e2ce3240c3c5805ed13919226dc7edb191
Billy Wood <billyc.wood4@gmail.com>
2025-04-06 16:12:34 -0600

    it works!

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -8,7 +8,7 @@ const initFirebaseAdmin = () => {
         initializeApp({
             credential: cert({
                 projectId: process.env.FIREBASE_PROJECT_ID,
-                clientEmail: process.env.NEXT_PUBLIC_FIREBASE_EMAIL,
+                clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
                 privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
             }),
         });
